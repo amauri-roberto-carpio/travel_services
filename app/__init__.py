389faf5cade25e2be44f62a40b0567b0e0200381
Amauri Carpio <amauri@ggbit.mx>
2025-11-03 10:04:47 -0600

    Se terminan de agregar los servicios de Service

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.extensions import db, migrate
 from config import Config
 
 from app.routes.users import users_bp
+from app.routes.services import services_bp
 
 def create_app():
     app = Flask(__name__)
@@ -12,5 +13,6 @@ def create_app():
     migrate.init_app(app, db)
 
     app.register_blueprint(users_bp)
+    app.register_blueprint(services_bp)
 
     return app
